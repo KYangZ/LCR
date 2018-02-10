0ba3956786b17147f91472930f0406f02b3641c7
klee1121 <klee1121@users.noreply.github.com>
2018-02-09 18:49:05 -0800

    Add files via upload
    -Player object possess dice
    -All dice objects are rolled individually within Player class
    -Heavily edited LCR class with code for a good portion of the game

--- GameRecord.java
+++ GameRecord.java
@@ -1,41 +1,51 @@
-
+import java.util.Scanner;
+import java.io.*;
+import java.io.FileWriter;
+import java.util.ArrayList;
+    
 /**
  * Write a description of class GameRecord here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
+public class GameRecord {
+    private ArrayList <Player> players = new ArrayList<Player>(); 
+    private String file;
 
-import java.util.Scanner;
-import java.io.*;
-import java.io.FileWriter;
-import java.util.ArrayList;
-
-public class GameRecord
-{
-    //public ArrayList<Player> players = new ArrayList<Player>();
-    
-    
-    
-    public GameRecord(){
+    public GameRecord(String file){
+        this.file = file;
     }
-    
-    public static void retrieveData(){
+
+    public ArrayList<Player> retrieveData(){
         try { 
-            Scanner sc = new Scanner(new File("LCRPlayers.txt"));
+            Scanner sc = new Scanner(new File(this.file));
             String str = "";
             String[] data;
+            int ID = 1;
             
             while(sc.hasNextLine()){
                 str = sc.nextLine();
                 data = str.split(",");
-                //Player name = new Player(data[0],);
-
+                Player name = new Player(data[0], 5, ID, Integer.parseInt(data[1]), Integer.parseInt(data[2]));
+                players.add(name);
+                ID++;
+               
             }
             sc.close();  //close the scanner
 
         } catch (IOException io) {
             System.out.println(io.getMessage());
         }
+        
+        
+        return players;
+        
     }
+    
+    public void updateRecord(/*pass parameters here */){
+        
+        
+    }
+    
 }
